735d42cf60a83f4e30fcf9e5efb83fc3696dade5
evan-kapantais <evankapantais@gmail.com>
2019-02-12 21:31:36 +0100

    tried stuff, didn't work

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ for (var i = 0; i < row.length; ++i) {
 
 function toggleSelected() {
 
+    // check if a row is selected; if yes, remove selection and select the one clicked
     for (var i = 0; i < row.length; ++i) {
 
         row[i].classList.remove('selected');
@@ -14,21 +15,20 @@ function toggleSelected() {
 
             row[i].lastChild.remove();
         }
-        
     }
 
     this.classList.add('selected');
 
+    // create checker element and add it to row
     var checker = document.createElement("img");
     checker.src = 'img/check.svg';
     checker.id = 'check';
     checker.alt = 'check';
-
     this.appendChild(checker);
 
     var rowNext =  document.getElementById('row-next');
     rowNext.style.backgroundColor = '#598BDB';
     rowNext.setAttribute('href', 'thanks.html');
-    
+
     console.log(checker);
 }
